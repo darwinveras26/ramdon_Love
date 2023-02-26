c8806941eea29758dd88c0045a7dacccf55a6642
pikachububble <darwinjverasteguis2002@gmail.com>
2023-02-26 13:19:32 -0500

    phrases were added

--- generator.js
+++ generator.js
@@ -1,3 +1,23 @@
-const randomLove = () => {
-  return console.log(Math.floor(Math.random()*9))
+
+const phraseLove = [
+   'I love you',
+   'You mean everything to me',
+   "I can't imagine my life without you",
+   "You complete me",
+   "I adore you",
+   "You are my soulmate",
+   "I am so lucky to have you in my life", 
+   'My love for you is unconditional', 
+   'You are the one Ive been waiting for', 
+   'I will always love you'
+]
+
+for (let i =0; i < phraseLove.length; i++ ) {
+
+}
+
+const randomLove = (phrase) => {
+  if (phrase <= 10) {
+    return console.log(Math.floor(Math.random()*9))
+  }
 } 
