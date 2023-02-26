01377487e14b566bc6eb331d5db26d58c88e5788
pikachububble <darwinjverasteguis2002@gmail.com>
2023-02-26 16:50:38 -0500

    code on the generator.js fixed

--- generator.js
+++ generator.js
@@ -1,16 +1,18 @@
 const phraseLove = [
-   'I love you',
-   'You mean everything to me',
-   "I can't imagine my life without you",
-   "You complete me",
-   "I adore you",
-   "You are my soulmate",
-   "I am so lucky to have you in my life", 
-   'My love for you is unconditional', 
-   'You are the one Ive been waiting for', 
-   'I will always love you'
+  'I love you',
+  'You mean everything to me',
+  "I can't imagine my life without you",
+  "You complete me",
+  "I adore you",
+  "You are my soulmate",
+  "I am so lucky to have you in my life", 
+  'My love for you is unconditional', 
+  'You are the one Ive been waiting for', 
+  'I will always love you'
 ]
 
-for (let i =0; i < phraseLove.length; i++ ) {
- console.log(phraseLove[i])
-}
+const ramdonPhrase = Math.floor(Math.random()* phraseLove.length) 
+
+const ramdonPhraseLove = phraseLove[ramdonPhrase] 
+
+console.log(ramdonPhraseLove)
